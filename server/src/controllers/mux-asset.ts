cd476c2bbdb7659b88df09548b7483413bd4a53f
Pavol Hudran <pavol.hudran@gmail.com>
2025-06-30 09:42:46 +0200

    fix: remove console logs

--- server/src/controllers/mux-asset.ts
+++ server/src/controllers/mux-asset.ts
@@ -41,11 +41,7 @@ const count = (ctx: Context) => {
 };
 
 const create = async (ctx: Context) => {
-  const body = ctx.request.body;
-
-  console.log('Create request body:', JSON.stringify(body));
-  console.log('Title value:', body?.title);
-  console.log('Title length:', body?.title?.length);
+  const { body } = ctx.request.body;
 
   return await strapi.documents(ASSET_MODEL).create({ data: body });
 };
